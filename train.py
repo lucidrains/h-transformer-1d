9c09a00a946ab12e0820f56ebaa21bfb8bb28fcf
Phil Wang <lucidrains@gmail.com>
2021-08-09 23:24:22 -0700

    fix an off by one

--- train.py
+++ train.py
@@ -62,7 +62,7 @@ class TextSamplerDataset(Dataset):
         self.seq_len = seq_len
 
     def __getitem__(self, index):
-        rand_start = torch.randint(0, self.data.size(0) - self.seq_len - 1, (1,))
+        rand_start = torch.randint(0, self.data.size(0) - self.seq_len, (1,))
         full_seq = self.data[rand_start: rand_start + self.seq_len + 1].long()
         return full_seq.cuda()
 
